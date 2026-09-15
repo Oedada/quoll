99c8ed00f57c7b6e5e255f1b8fc190ba73635e7b
Oedada <Oedada>
2026-09-15 23:03:47 +0300

    add example with keycloak in backend/examples/keycloak

---
+++ backend/examples/keycloak/test.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+import json
+import secrets
+
+import requests
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+
+KEYCLOAK_URL = "http://localhost:8081"
+REALM = "demo"
+REALM_URL = f"{KEYCLOAK_URL}/realms/{REALM}"
+BASE_URL = f"{REALM_URL}/protocol/openid-connect"
+CLIENT_ID = "demo-site"
+REDIRECT_URI = "http://localhost:8000/callback"
+sessions = {}
+print("http://localhost:8000/auth")
+app = FastAPI()
+
+@dataclass
+class Tokens:
+    access: str
+    refresh: str
+
+
+@app.get("/")
+def root(req: Request):
+    print(sessions.get(req.cookies.get('session')))
+    return "Main page"
+
+
+@app.get("/auth")
+def auth():
+    url = (
+        f"{BASE_URL}/auth"
+        f"?client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URI}"
+        f"&response_type=code"
+        f"&scope=openid"
+    )
+    return RedirectResponse(url)
+
+
+@app.get("/callback")
+def callback(code: str):
+    auth_resp = requests.post(
+        f"{BASE_URL}/token",
+        headers={"content-type": "application/x-www-form-urlencoded"},
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": REDIRECT_URI,
+            "client_id": CLIENT_ID,
+        },
+    )
+    auth_data = json.loads(auth_resp.content)
+    session_id = secrets.token_urlsafe(64)
+    sessions[session_id] = Tokens(auth_data["access_token"], auth_data["refresh_token"])
+    resp = requests.get(
+        f"{BASE_URL}/userinfo",
+        headers={"Authorization": f"Bearer {auth_data['access_token']}"},
+    )
+    print("\n".join([f"{k}: {v}" for k, v in json.loads(resp.content).items()]))
+    response = RedirectResponse("/")
+    response.set_cookie("session", session_id, httponly=True)
+    return response
